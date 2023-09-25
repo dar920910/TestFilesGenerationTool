95894466ceb2c68c5b4222aa2006703b2f171645
Denis Rezepov <me@dar920910.ru>
2023-09-25 08:06:57 +0300

    Code from DateTimeService was moved to the CustomFileObject class definition.

--- TestFilesGenerator.Library/src/CustomFileObject.cs
+++ TestFilesGenerator.Library/src/CustomFileObject.cs
@@ -61,8 +61,98 @@ public class CustomFileObject
     private static string GetCloningErrorMessage(string idName) =>
         $"   [ERROR]: {idName} cannot be created via cloning.";
 
+    private static string GetFileObjectCreationDateTime()
+    {
+        string sep = " - ";
+        string dateSep = ".";
+        string timeSep = ".";
+        string zero = "0";
+        bool ms = true;
+
+        return GetDateAndTime(sep, dateSep, timeSep, ms, zero);
+    }
+
+    private static string GetDateAndTime(string sep, string dateSep, string timeSep, bool ms, string zero)
+    {
+        string datetime, date, time;
+
+        date = GetDate(dateSep, zero);
+        time = GetTime(timeSep, ms, zero);
+
+        datetime = date + sep + time;
+
+        return datetime;
+    }
+
+    private static string GetDate(string sep, string zero)
+    {
+        string date, dtYear, dtMonth, dtDay;
+        int year, month, day;
+
+        year = DateTime.Now.Year;
+        month = DateTime.Now.Month;
+        day = DateTime.Now.Day;
+
+        dtYear = GetDateOrTimeWithZero(year, zero);
+        dtMonth = GetDateOrTimeWithZero(month, zero);
+        dtDay = GetDateOrTimeWithZero(day, zero);
+
+        date = dtYear + sep + dtMonth + sep + dtDay;
+
+        return date;
+    }
+
+    private static string GetTime(string sep, bool ms, string zero)
+    {
+        string time, tmHour, tmMinute, tmSecond;
+        int hour, minute, second;
+
+        hour = DateTime.Now.Hour;
+        minute = DateTime.Now.Minute;
+        second = DateTime.Now.Second;
+
+        tmHour = GetDateOrTimeWithZero(hour, zero);
+        tmMinute = GetDateOrTimeWithZero(minute, zero);
+        tmSecond = GetDateOrTimeWithZero(second, zero);
+
+        time = tmHour + sep + tmMinute + sep + tmSecond;
+
+        if (ms)
+        {
+            int millisecond;
+            string tmMillisecond;
+
+            millisecond = DateTime.Now.Millisecond;
+            tmMillisecond = GetMillisecondWithZero(millisecond, zero);
+
+            time += sep + tmMillisecond;
+        }
+
+        return time;
+    }
+
+    private static string GetDateOrTimeWithZero(int datetimeValue, string zero)
+    {
+        return (datetimeValue < 10) ? zero + datetimeValue.ToString() : datetimeValue.ToString();
+    }
+
+    private static string GetMillisecondWithZero(int millisecond, string zero)
+    {
+        if (millisecond < 10)
+        {
+            return zero + zero + millisecond.ToString();
+        }
+
+        if (millisecond < 100)
+        {
+            return zero + millisecond.ToString();
+        }
+
+        return millisecond.ToString();
+    }
+
     private string GetCloningResultMessage(string idName) =>
-        $"   [{DateTimeService.GetDateAndTimeDefaultString()}]: {idName}\n" +
+        $"   [{GetFileObjectCreationDateTime()}]: {idName}\n" +
         $"   [!] Available Space: {FileDriveManager.GetFreeDiskSpaceOnStorage(this)} bytes.";
 }
 
